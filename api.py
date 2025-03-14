0c74bdc9a1918e85255eefd682d35ebedb4e1af0
Feals <ax.juste@gmail.com>
2025-03-14 16:22:46 +0100

    connexion with front

--- api.py
+++ api.py
@@ -2,26 +2,32 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib  # Pour charger le modèle ML
 import numpy as np
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Permet à React d’accéder à l’API
 
 # Charger le modèle ML et le vectorizer
-model_classifier = joblib.load("model_grid_search_classifier.pkl")
-model_regressor = joblib.load("dream_model_regressor.pkl")  # Si ton modèle utilise du texte
+model_classifier = joblib.load("model_grid_search_classifier_pipeline.pkl")
+model_regressor = joblib.load("model_grid_search_regressor_pipeline.pkl")  # Si ton modèle utilise du texte
 vectorizer = joblib.load("preprocessor_pipeline.pkl")
 
 
 
 @app.route("/classificationDream/request", methods=["POST"])
 def classify_dream():
+    print("request.get_json()", request.get_json())
     try:
         data = request.get_json()
-        dream_text = data.get("dream")  # Récupération de la description du rêve
-
+        dream_text = data.get("descriptionDream")  # Récupération de la description du rêve
+        print("dream_text", dream_text)
         if not dream_text:
             return jsonify({"error": "No dream provided"}), 400
 
+        '''
         # Transformer le texte en features exploitables par le modèle
         dream_vector = vectorizer.transform([dream_text])
 
@@ -34,9 +40,11 @@ def classify_dream():
         confidence_regressor = model_regressor.predict_proba(dream_vector).max()
 
         return jsonify({"prediction_classifier": prediction_classifier, "confidence_classifier": confidence_classifier, "prediction_regressor": confidence_regressor, "confidence": confidence_regressor})
-
+        '''
+        return jsonify("réponse api")
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = os.getenv("PORT", 5000)
+    app.run(debug=True, host="127.0.0.1", port=int(port))
